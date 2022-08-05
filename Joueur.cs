addb43863eb483cd5e78836c3bb350633ff90111
Moustafa moungari <moungari.moustafa@gmail.com>
2022-08-05 13:32:33 +0200

    Gestion des gains et verification si le joueur qui a le plus de points n'est pas le Donneur

--- Joueur.cs
+++ Joueur.cs
@@ -10,23 +10,24 @@ namespace BlackJack
     public class Joueur :MainJoueur , IMain
     {
         private string _nom = string.Empty;
-        private int _argent = 0;
-        private int _mise = 0;
+        private double _argent = 0;
+        private double _mise = 0;
         private bool _isCroupier = false;
         private string _erreurMise = "la mise ne peut etre superieur au montant que possede le joueur";
         private string _isMiseNull = "la mise doit etre superieur a 0";
         private string _blackJackMessage = "Vous etes le vainqueur ! Congratulations ";
-        private int _gain = 0;
+        private double _gain = 0;
         private int Max = 0;
 
         private static Dictionary<Joueur, List<string[]>> dicJoueur = new Dictionary<Joueur, List<string[]>>();
+
         private static List<Joueur> lstJoueurs = new List<Joueur>();
         public string Nom { get { return _nom; } set { this._nom = value; } }
-        public int Argent { get { return _argent; } set { this._argent = value; } }
-        public int Mise { get { return _mise; } set { this._mise = value; } }
+        public double Argent { get { return _argent; } set { this._argent = value; } }
+        public double Mise { get { return _mise; } set { this._mise = value; } }
         public bool Croupier { get { return _isCroupier; } set { this._isCroupier = value;} }
 
-        public int Gain { get { return _gain; } set { this._gain = value; } }
+        public double Gain { get { return _gain; } set { this._gain = value; } }
 
 
         public string ErreurMise { get { return _erreurMise; } }
@@ -55,7 +56,7 @@ namespace BlackJack
                         $"  Valeur => {item.Value[0][1].ToString()} " +
                         //$"  Carte 2: {item.Value[0][2].ToString()} " +
                         //$"  Valeur =>{item.Value[1][3].ToString()} " +
-                        $"  Total : {item.Key.GetPointCarteMainJoueur} ");
+                        $"  Total : {item.Key.GetPointCarteMainJoueur} points ");
                    
                 }
 
@@ -71,6 +72,7 @@ namespace BlackJack
                 {
                     if (MainJoueur.IsBlackJack)
                     {
+
                        if(j.Key.GetPointCarteMainJoueur == 21)
                         {
                             MainJoueur.Joueur = j.Key;
@@ -82,15 +84,17 @@ namespace BlackJack
                             Console.WriteLine();
                             Console.WriteLine();
                         }
+
                       
                     }
-                    Console.WriteLine($"{j.Key.Nom} => {j.Key.GetPointCarteMainJoueur}");
-                    WinnerPlayer(j.Key, j.Key.GetPointCarteMainJoueur);
+                    Console.WriteLine($"{j.Key.Nom} => {j.Key.GetPointCarteMainJoueur} points");
 
+                    //WinnerPlayer(j.Key, j.Key.GetPointCarteMainJoueur);
+                    lstJoueurs.Add(j.Key);
                 }
 
 
-
+                playerPointMoreThanDonneur();
 
             }
             else
@@ -139,18 +143,84 @@ namespace BlackJack
 
            if( Max < points) { Max = points; }
 
+           
+
             if (Max >= points && j.GetPointCarteMainJoueur >= Max) 
             {
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine("----------- WINNER IS ------------------------");
-                Console.WriteLine($" Nom : {j.Nom}  Gain : {j.Gain}  Points : {Max}");
+                Console.WriteLine($" Nom : {j.Nom}  Gain : {j.Gain}  Points : {Max} Argent : {j.Argent}");
                 Console.WriteLine("----------------------------------------------");
                 Console.WriteLine();
                 Console.WriteLine();
             }
 
            
+        }
+
+
+       
+        //Verifie si le joueur qui a le plus de points n'est pas le Donneur
+        public void playerPointMoreThanDonneur()
+        {
+            var isNotCroupier = lstJoueurs.Where(j => j._isCroupier != true).ToList();
+            var isCroupier = lstJoueurs.Where(j => j._isCroupier == true).FirstOrDefault();
+
+            foreach (var j in isNotCroupier)
+            {
+                if(isCroupier != null)
+                {
+                    if (j.GetPointCarteMainJoueur > isCroupier.GetPointCarteMainJoueur)
+                    {
+
+                        RealizeGain(j);
+                        WinnerPlayer(j, j.GetPointCarteMainJoueur);
+                    }
+                }
+               
+            }
+        }
+
+
+        //Mise a jour des mises des joueurs
+        public void UpdateOfPlayerMise()
+        {
+
+        }
+
+
+        //Calcul du gain
+        public void RealizeGain(Joueur j)
+        {
+            if(j != null)
+            {
+                switch (j.GetPointCarteMainJoueur)
+                {
+
+                    case 21:
+                        j.Gain = (1.5 * j.Mise);
+                        j.Argent = j.Argent + j.Gain;
+                        break;
+
+                    case 20:
+                        j.Gain = (0.5 * j.Mise);
+                        j.Argent = j.Argent + j.Gain;
+                        break;
+
+                    case 15: 
+                    case 16: 
+                    case 17:
+                    case 18:
+                    case 19:
+                        j.Gain = (0.2 * j.Mise);
+                        j.Argent = j.Argent + j.Gain;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
         }
        
     }
